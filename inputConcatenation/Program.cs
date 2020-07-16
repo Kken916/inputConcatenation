9d57f8d3699b352af82641c2cf53194572ff936f
Kenny Ingram <68390035+Kken916@users.noreply.github.com>
2020-07-16 14:28:27 -0400

    Add files via upload

---
+++ inputConcatenation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inputConcatenation
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Define Variables first
+            string firstName, lastName, fullName;
+
+            Console.WriteLine("Please enter your first name.");
+            firstName = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("please enter your last name.");
+            lastName = Convert.ToString(Console.ReadLine());
+
+            //concatenation
+            fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+            Console.Read();
+        }
+    }
+}
